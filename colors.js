1a747c904ada5b6ef82aeb1bc2ea47870918508b
Drorbanzsa <drorbanzsa@tuta.com>
2025-08-11 09:52:13 +0200

    Update colors.js

--- colors.js
+++ colors.js
@@ -5,10 +5,11 @@ export const CLUSTER_COLORS={
   3:'#984ea3',  // városi
   4:'#e41a1c'   // turizmus
 };
-export const ICON_FILES={
-  0:'assets/icons/buza.svg',
-  1:'assets/icons/fogaskerek.svg',
-  2:'assets/icons/aso.svg',
-  3:'assets/icons/gazdag.svg',
-  4:'assets/icons/ernyo.svg'
+export const ICON_FILES = {
+  0: "buza.svg",
+  1: "fogaskerek.svg",
+  2: "gazdag.svg",
+  3: "ernyo.svg",
+  4: "aso.svg"
 };
+
