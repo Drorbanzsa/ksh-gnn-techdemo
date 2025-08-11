a36952c5512b6264e64a15e6ec8401f8de7cf831
Drorbanzsa <drorbanzsa@tuta.com>
2025-08-11 15:13:30 +0200

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,17 +1,23 @@
 export const CLUSTER_COLORS = {
-  0: "#52b788",  // zöld
-  1: "#bdbdbd",  // szürke
-  2: "#e76f51",  // piros
-  3: "#8d6e63",  // barna
-  4: "#f4a261"   // narancs
+  0: "#52b788", // Falusi agrár-periféria
+  1: "#f4a261", // Turizmus-/üdülő-öv
+  2: "#e76f51", // Közmunka-intenzív periféria
+  3: "#8d6e63", // Vállalkozó vidéki (agrár-mix)
+  4: "#bdbdbd"  // Városi ipar-szolgáltató központok
 };
 
-// Ha Kecskemét „nem napernyős”, itt tudod áttetni az ikon-mappinget
 export const ICON_FILES = {
-  0: "aso.svg",        // pl. agrár
-  1: "ernyo.svg",  // ipari/vegyes
-  2: "fogaskerek.svg",      // városi/fejlett
-  3: "buza.svg",         // (volt 'ernyo', átírható)
-  4: "gazdag.svg"       // turisztika/rekreáció
+  0: "buza.svg",
+  1: "ernyo.svg",
+  2: "aso.svg",
+  3: "fogaskerek.svg",
+  4: "gazdag.svg"
 };
- 
+
+export const CLUSTER_LABELS = {
+  0: "Falusi agrár‑periféria",
+  1: "Turizmus‑/üdülő‑öv",
+  2: "Közmunka‑intenzív periféria",
+  3: "Vállalkozó vidéki öv",
+  4: "Városi ipar‑szolgáltató központok"
+};
