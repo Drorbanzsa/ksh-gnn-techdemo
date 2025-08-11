eb4186bff8673731b9ca8b4d69439e7773524f95
Drorbanzsa <drorbanzsa@tuta.com>
2025-08-11 11:32:08 +0200

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,15 +1,17 @@
-export const CLUSTER_COLORS={
-  0:'#4daf4a',  // agrár
-  1:'#377eb8',  // ipar‑agrár
-  2:'#ff7f00',  // szociális
-  3:'#984ea3',  // városi
-  4:'#e41a1c'   // turizmus
-};
-export const ICON_FILES = {
-  0: "buza.svg",
-  1: "fogaskerek.svg",
-  2: "gazdag.svg",
-  3: "ernyo.svg",
-  4: "aso.svg"
+export const CLUSTER_COLORS = {
+  0: "#52b788",  // zöld
+  1: "#bdbdbd",  // szürke
+  2: "#e76f51",  // piros
+  3: "#8d6e63",  // barna
+  4: "#f4a261"   // narancs
 };
 
+// Ha Kecskemét „nem napernyős”, itt tudod áttetni az ikon-mappinget
+export const ICON_FILES = {
+  0: "fogaskerek.svg",        // pl. agrár
+  1: "ernyo.svg",  // ipari/vegyes
+  2: "aso.svg",      // városi/fejlett
+  3: "buza.svg",         // (volt 'ernyo', átírható)
+  4: "gazdag.svg"       // turisztika/rekreáció
+};
+ 
